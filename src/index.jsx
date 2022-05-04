7988f1212b870f74eb7b0bf492b19efb717a6ca9
tonnyhawk <tonnyhawk93@gmail.com>
2022-05-05 01:24:35 +0300

    step 11

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,34 @@
-// @ts-check
+// @ts-nocheck
 
 import React from 'react';
 import App from './components/App.jsx';
 import ReactDom from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { Provider as Rollbar, ErrorBoundary } from '@rollbar/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './slices'
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
 
 const root = document.querySelector('#chat');
+const rollbarConfig = {
+    accessToken: "bc120308c8f84615a5ba5d4b0098d5ba",
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+        environment: "production"
+    }
+};
 
 ReactDom.render(
-    <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
-    </Provider>,
-root)
+    <Rollbar config={rollbarConfig}>
+        <ErrorBoundary>
+            <Provider store={store}>
+                <Router>
+                    <App />
+                </Router>
+            </Provider>
+        </ErrorBoundary>
+    </Rollbar>,
+    root)
