5c03fefe8ff07e75aceff53f42181aa6dcad7138
tonnyhawk <tonnyhawk93@gmail.com>
2022-05-05 03:53:23 +0300

    step 12

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-import PageLayout from "../../components/PageLayout";
-import Container from "../../components/Container";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PageLayout from '../../components/PageLayout';
+import Container from '../../components/Container';
 
-const NotFoundPage = () => {
-    const {t} = useTranslation();
+function NotFoundPage() {
+  const { t } = useTranslation();
 
-    return <PageLayout>
-        <Container>
-            <div className="row justify-content-center align-content-center h-100">
-                <div className="col-4">
-                    <h2>{t('notFound.title')}</h2>
-                    <span>{t('notFound.message')}<Link to="/">{t('notFound.linkText')}</Link></span>
-                </div>
-            </div>
-        </Container>
+  return (
+    <PageLayout>
+      <Container>
+        <div className="row justify-content-center align-content-center h-100">
+          <div className="col-4">
+            <h2>{t('notFound.title')}</h2>
+            <span>
+              {t('notFound.message')}
+              <Link to="/">{t('notFound.linkText')}</Link>
+            </span>
+          </div>
+        </div>
+      </Container>
     </PageLayout>
+  );
 }
 
 export default NotFoundPage;
