3c230dc6c4e77b0bb9eaac7c5a35f257a782c193
tonnyhawk <tonnyhawk93@gmail.com>
2022-05-05 03:43:22 +0300

    step 12

--- src/components/ChatPanel/ChatPanel.jsx
+++ src/components/ChatPanel/ChatPanel.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ChatHeader from './components/ChatHeader';
-import MessagesBox from "./components/MessagesBox";
+import MessagesBox from './components/MessagesBox';
 import ChatForm from './components/ChatForm';
 
-const ChatPanel = ({channelName, messages, currentChannelId}) => {
-    return (
-        <div className='col p-0 h-100'>
-            <div className='d-flex flex-column h-100'>
-                <ChatHeader channelName={channelName} messagesCount={messages.length}/>
-                <MessagesBox messages={messages} />
-                <ChatForm currentChannelId={currentChannelId}/>
-            </div>
-        </div>
-    )
+function ChatPanel({ channelName, messages, currentChannelId }) {
+  return (
+    <div className="col p-0 h-100">
+      <div className="d-flex flex-column h-100">
+        <ChatHeader channelName={channelName} messagesCount={messages.length} />
+        <MessagesBox messages={messages} />
+        <ChatForm currentChannelId={currentChannelId} />
+      </div>
+    </div>
+  );
 }
 
 export default ChatPanel;
